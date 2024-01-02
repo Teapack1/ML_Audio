951bb425e7be6fe7986baf59d2c1670d889c869e
Teapack1 <ondrej.major@gmail.com>
2024-01-02 14:03:41 +0100

    update

--- Transformers/FastAPI_assistant/static/script.js
+++ Transformers/FastAPI_assistant/static/script.js
@@ -1,18 +1,37 @@
 let ws;
 
-function startWebSocket() {
-    ws = new WebSocket("ws://localhost:8000/ws");
-    ws.onmessage = function(event) {
-        const serverMessage = event.data;
-        document.getElementById('results').innerText = serverMessage;
-        if (serverMessage === "Restarting system...") {
-            displayRestartButton();
-        }
-    };
+function toggleButton(isStart) {
+    const startButton = document.getElementById('startBtn');
+    if (isStart) {
+        startButton.innerText = "Start Recording";
+        startButton.className = "start-button"; // CSS class for the start button
+    } else {
+        startButton.innerText = "Stop";
+        startButton.className = "stop-button"; // CSS class for the stop button
+    }
 }
 
 function startRecording() {
-    ws.send("Start recording"); // This message would trigger the server to start the audio processing
+    // Initialize WebSocket connection only if it's not already established
+    if (!ws || ws.readyState === WebSocket.CLOSED) {
+        ws = new WebSocket("ws://localhost:8000/ws");
+
+        ws.onopen = function(event) {
+            ws.send("Start recording"); // Trigger the server to start the audio processing
+        };
+
+        ws.onmessage = function(event) {
+            const serverMessage = event.data;
+            document.getElementById('results').innerText = serverMessage;
+            if (serverMessage.includes("Wake word detected")) {
+                toggleButton(false); // Change to "Stop" button
+            }
+            if (serverMessage === "Restarting system...") {
+                toggleButton(true); // Change back to "Start" button
+                displayRestartButton();
+            }
+        };
+    }
 }
 
 function displayRestartButton() {
@@ -21,9 +40,7 @@ function displayRestartButton() {
     restartBtn.onclick = function() {
         location.reload(); // Reload the page to restart the process
     };
-    document.body.appendChild(restartBtn);
+    document.getElementById('restart-button-container').appendChild(restartBtn);
 }
 
 document.getElementById('startBtn').addEventListener('click', startRecording);
-
-window.onload = startWebSocket;
